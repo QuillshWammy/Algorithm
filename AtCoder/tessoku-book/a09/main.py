41ad01fce136b1dba8d97607458c939c9f42e252
QuillshWammy <watariedit008@gmail.com>
2023-05-10 12:14:48 +0900

    add tips

--- AtCoder/tessoku-book/a09/main.py
+++ AtCoder/tessoku-book/a09/main.py
@@ -1,11 +1,26 @@
 import sys
 
 def main(lines):
-    # 処理
-    print(lines)
-        
+    H, W, N = lines[0]
+    snow_lst = lines[1:]
+    
+    X = [[0]*(W+2) for _ in range(H+2)]
+    
+    for snow in snow_lst:
+        a, b, c, d = snow
+        X[a][b] += 1
+        X[c+1][d+1] += 1
+        X[c+1][b] -= 1
+        X[a][d+1] -= 1
+    
+    for row in range(H):
+        for axe in range(W):
+            X[row+1][axe+1] += X[row][axe+1]+X[row+1][axe]-X[row][axe]
+    
+    for R in X[1:-1]:print(*R[1:-1])
+            
 if __name__ == '__main__':
     lines = []
     for l in sys.stdin:
-        lines.append(l.rstrip('\r\n'))
+        lines.append(list(map(int, l.split())))
     main(lines)
--- AtCoder/tessoku-book/b09/main.py
+++ AtCoder/tessoku-book/b09/main.py
@@ -1,8 +1,32 @@
 import sys
 
 def main(lines):
-    print(lines)
-        
+    N = lines[0][0]
+    points = lines[1:]
+    s = [[0]*1501 for _ in range(1501)]
+    
+    for point in points:
+        a, b, c, d = point
+        s[a][b] += 1
+        s[a][d] -= 1
+        s[c][b] -= 1
+        s[c][d] += 1
+    
+    for row in range(1501):
+        for axe in range(1, 1501):
+            s[row][axe] += s[row][axe-1]
+    
+    for axe in range(1501):
+        for row in range(1, 1501):
+            s[row][axe] += s[row-1][axe]
+    
+    cnt = 0
+    for row in range(1501):
+        for axe in range(1501):
+            if s[row][axe] >= 1:
+                cnt += 1
+    print(cnt)
+    
 if __name__ == '__main__':
     lines = []
     for l in sys.stdin:
