3b091c94c6041f29948afa3f98b26aa361f19966
QuillshWammy <watariedit008@gmail.com>
2023-05-19 23:58:07 +0900

    add tips

--- AtCoder/tessoku-book/b12/main.py
+++ AtCoder/tessoku-book/b12/main.py
@@ -1,7 +1,18 @@
 import sys
 
 def main(lines):
-    print(lines)
+    N = lines[0][0]
+    
+    srt = 0
+    end = N
+    
+    while srt <= end:
+        mid = (srt+end)//2
+        res = mid ** 3 + mid
+        if res >= N:
+            srt = mid+1
+        else:
+            end = mid-1
         
 if __name__ == '__main__':
     lines = []
