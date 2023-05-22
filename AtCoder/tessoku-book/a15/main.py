743a66830b27fa05deba9149f834f82f2fb23fec
QuillshWammy <watariedit008@gmail.com>
2023-05-22 18:58:05 +0900

    add tips

--- AtCoder/tessoku-book/a15/main.py
+++ AtCoder/tessoku-book/a15/main.py
@@ -1,7 +1,21 @@
 import sys
+import bisect
 
 def main(lines):
-    print(lines)
+    N = lines[0][0]
+    A = lines[1]
+
+    X = list(set(A))
+    X.sort()
+
+    B = [None] * N
+    for i in range(N):
+        B[i] = bisect.bisect_left(X, A[i])
+        B[i] += 1
+    
+    ans = [str(i) for i in B]
+    print(" ".join(ans))
+        
         
 if __name__ == '__main__':
     lines = []
